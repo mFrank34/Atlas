2cea3b403bddbeda43566d570469f230373a6fa7
Michael Frank <mffranks34@gmail.com>
2025-12-14 20:04:37 +0000

    changes to Skills.h

--- source/include/Skills.h
+++ source/include/Skills.h
@@ -1,8 +1,12 @@
 #ifndef SKILLS_H
 #define SKILLS_H
 
+#include <vector>
 #include <string>
 
+/**
+ * Ability for different stats in relation
+ */
 enum class Ability {
     Strength,
     Dexterity,
@@ -12,6 +16,10 @@ enum class Ability {
     Charisma
 };
 
+/**
+ * Element for a skill with it
+ * with different types include
+ */
 struct Skill {
     std::string name;
     Ability ability;
@@ -19,12 +27,33 @@ struct Skill {
     bool isProficient;
 };
 
+/**
+ * Manager for Skill
+ */
 class Skills
 {
 private:
-
+    std::vector<Skill> skills;
 public:
+    /**
+     * generates list of skills for use for displaying
+     * @param strength stat block
+     * @param dexterity stat block
+     * @param constitution stat block
+     * @param intelligence stat block
+     * @param wisdom stat block
+     * @param Charisma stat block
+     */
+    skills(int strength, int dexterity, int constitution,
+           int intelligence, int wisdom, int Charisma);
 
+    /**
+     * returns the map of skills
+     * @return returns the map of skills
+     */
+    std::vector<Skill>& getSkills();
+
+    ~Skills() = default;
 };
 
 #endif
