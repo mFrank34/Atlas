7a72b5324bb6eb85a240e61388b919366ce416d5
Michael Frank <mffranks34@gmail.com>
2026-01-09 20:44:11 +0000

    adding a pd handler for application

---
+++ src/handler/PDManager.java
@@ -0,0 +1,77 @@
+package handler;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+import org.apache.pdfbox.pdmodel.interactive.form.PDNonTerminalField;
+import utilities.Constants;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PDManager {
+
+    /**
+     * Checking the key of the PDF to match the application signature
+     *
+     * @param file caricature sheet location
+     * @return bool if the document contains right sig
+     */
+    private static boolean compatibilityChecker(PDDocument file) {
+        PDDocumentInformation info = file.getDocumentInformation();
+        String signature = info.getCustomMetadataValue(Constants.SIGNATURE_KEY);
+        return Constants.EXPECTED_SIGNATURE.equals(signature);
+    }
+
+    /**
+     * Checking the fields from a PDF and converting them into a map system
+     * @param fields the name of the field
+     * @param data the current data set
+     * @throws IOException to throw an error if something breaks you know
+     */
+    private static void readFields(List<PDField> fields, Map<String, String> data)
+            throws IOException {
+        for (PDField field : fields) {
+            if (field instanceof PDNonTerminalField nonTerminal) {
+                readFields(nonTerminal.getChildren(), data);
+            } else {
+                data.put(
+                        field.getFullyQualifiedName(),
+                        field.getValueAsString() != null ? field.getValueAsString() : ""
+                );
+            }
+        }
+    }
+
+    /**
+     * data from the PDF file that has been Read into application
+     * @param file location of the file
+     * @return returns data in manageable format
+     * @throws IOException returns error if file breaks
+     */
+    public static Map<String, String> loadFromPdf(File file)
+            throws IOException {
+        Map<String, String> data = new HashMap<>();
+        try (PDDocument document = PDDocument.load(file)) {
+            PDAcroForm form = document.getDocumentCatalog().getAcroForm();
+            if (form == null) {
+                throw new IOException("PDF has no form fields");
+            }
+
+            if (!compatibilityChecker(document)) {
+                throw new IOException("PDF has incompatible form fields");
+            }
+
+            readFields(form.getFields(), data);
+        }
+        return data;
+    }
+
+    public static void saveToPdf(File file, Character character) {
+
+    }
+}
--- src/utilities/Constants.java
+++ src/utilities/Constants.java
@@ -1,4 +1,15 @@
 package utilities;
 
 public class Constants {
+    public static final String SIGNATURE_KEY = "Atlas-Signature";
+    public static final String EXPECTED_SIGNATURE = "ATLAS_V1";
+
+
+
+    /**
+     * private constructor to prevent initialization as nothing should create this class
+     */
+    private Constants() {
+        throw new AssertionError("Cannot instantiate constants class");
+    }
 }
